12bcba1fb9f36e97eabecff1241df1e66a752b64
IROpen <hamid.6.11.79@gmail.com>
2019-09-09 15:34:13 +0430

    move to github

---
+++ index.js
@@ -0,0 +1,68 @@
+function isTopLevel(node){
+  let scope = node.parent;
+  while (scope.type === 'BlockStatement'){
+    scope = scope.parent;
+  }
+  return (scope.type === 'Program');     
+}
+
+function se(context){
+  return node=>{
+    if (isTopLevel(node)){
+      context.report({
+        node,
+        message: `Side effects in toplevel are not allowed.`,
+      })
+    }
+  }
+}
+
+module.exports = {
+  rules: {
+    'no-toplevel-var': function(context){
+      return {
+        "VariableDeclaration": function(node) {
+          if (node.kind === "var") {
+            if(isTopLevel(node)){
+              context.report({
+                node, 
+                message: `Unexpected var in toplevel, use const.`,
+              });
+            }
+          }
+        }
+      };
+    },
+    'no-toplevel-let': function(context){
+      return {
+        "VariableDeclaration": function(node) {
+          if (node.kind === "let") {
+            if(isTopLevel(node)){
+              context.report({
+                node, 
+                message: `Unexpected let in toplevel, use const.`,
+              });
+            }
+          }
+        }
+      };
+    },
+    'no-toplevel-side-effect': (context)=>({
+      ExpressionStatement:se(context),
+      IfStatement:se(context),
+      ForStatement:se(context),
+      WhileStatement:se(context),
+      SwitchStatement:se(context),
+    }),
+    'no-toplevel-block': (ctx)=>({
+      BlockStatement(node){
+        if (isTopLevel(node)){
+          ctx.report({
+            node,
+            message: `Blocks in toplevel are not allowed.`
+          })
+        }
+      }
+    })
+  }
+}
